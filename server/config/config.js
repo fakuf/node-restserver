5a281665967150925c60b3b6b688b5920839b33a
fakuf <faku_f@hotmail.com>
2019-07-15 09:09:31 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -7,10 +7,10 @@ process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
-//   urlDB = 'mongodb://localhost:27017/cafe'
-//} else {
-urlDB = 'mongodb+srv://facundofalcone16:CqK0WNLF87Z3c7AU@cluster0-crhdc.mongodb.net/cafe'
-    //}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://facundofalcone16:CqK0WNLF87Z3c7AU@cluster0-crhdc.mongodb.net/cafe'
+}
 
 process.env.URLDB = urlDB;
